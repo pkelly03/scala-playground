21b23bea46775325da1196d168ae0b2ae917456a
Paul Kelly <pkelly@equalexperts.com>
2015-01-25 16:15:26 +0000

    cake pattern

--- src/main/scala/com/designpatterns/CakePattern.scala
+++ src/main/scala/com/designpatterns/CakePattern.scala
@@ -1,5 +1,30 @@
 package com.designpatterns
 
-class CakePattern {
-
+object CakePattern extends App {
+  // example didn't work
+  Env.welcome
 }
+
+trait Context
+
+// Where the dependency injection occurs
+trait MyContext extends Context {
+  this: Config =>
+
+  def welcome = this.text
+}
+
+trait Config {
+  load
+  def load: Unit
+  val text: String
+}
+
+case class InMemoryConfig() extends Config {
+  val text = "Hello"
+  def load = println("load : " + text)
+}
+
+object Env extends InMemoryConfig with MyContext
+
+
